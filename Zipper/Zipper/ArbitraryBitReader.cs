e1783eb7ddcf61d19834ae0350fc04f3fa20f5ae
ilya-krivtsov <180809461+ilya-krivtsov@users.noreply.github.com>
2025-03-02 07:36:29 +0300

    Homework 3 - LZW (wip)
    Added arbitrary bit length writer/reader

---
+++ Zipper/Zipper/ArbitraryBitReader.cs
@@ -0,0 +1,67 @@
+namespace Zipper;
+
+using System.Diagnostics;
+
+/// <summary>
+/// Reads integers of arbitrary width.
+/// </summary>
+internal class ArbitraryBitReader
+{
+    private readonly Stream stream;
+    private readonly int width;
+    private byte buffer;
+    private int? bitsReadFromBuffer;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ArbitraryBitReader"/> class.
+    /// </summary>
+    /// <param name="stream">Stream to write to.</param>
+    /// <param name="width">Width of integers between 4 and 32 bits.</param>
+    public ArbitraryBitReader(Stream stream, int width)
+    {
+        ArgumentOutOfRangeException.ThrowIfLessThan(width, 4, nameof(width));
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(width, 32, nameof(width));
+
+        this.stream = stream;
+        this.width = width;
+        bitsReadFromBuffer = null;
+    }
+
+    /// <summary>
+    /// Reads number from underlying stream and stores it in the <paramref name="number"/>.
+    /// </summary>
+    /// <param name="number">When this method returns, contains the value that was read, if read successfully; otherwise, zero.</param>
+    /// <returns><see langword="true"/> if <paramref name="number"/> was successfuly read, <see langword="true"/> otherwise.</returns>
+    public bool ReadNext(out int number)
+    {
+        number = 0;
+
+        int remainingWidth = width;
+        while (remainingWidth > 0)
+        {
+            if (bitsReadFromBuffer is null or 0 or >= 8)
+            {
+                int readByte = stream.ReadByte();
+                if (readByte == -1)
+                {
+                    return false;
+                }
+
+                buffer = (byte)readByte;
+                bitsReadFromBuffer = 0;
+            }
+
+            int remainingBitsToRead = 8 - bitsReadFromBuffer.Value;
+            int mask = 0xFF >> bitsReadFromBuffer.Value;
+            int toWrite = (buffer & mask) >> Math.Max(0, remainingBitsToRead - remainingWidth);
+
+            int previousRemainingWidth = remainingWidth;
+            remainingWidth -= remainingBitsToRead;
+            remainingWidth = Math.Max(0, remainingWidth);
+            bitsReadFromBuffer += previousRemainingWidth - remainingWidth;
+            number |= toWrite << remainingWidth;
+        }
+
+        return true;
+    }
+}
---
+++ Zipper/Zipper/ArbitraryBitWriter.cs
@@ -0,0 +1,97 @@
+namespace Zipper;
+
+using System.Diagnostics;
+
+/// <summary>
+/// Writes integers of arbitrary width.
+/// </summary>
+internal class ArbitraryBitWriter : IDisposable
+{
+    private readonly Stream stream;
+    private readonly int width;
+    private readonly byte[] buffer;
+    private int bitsWrittenInBuffer;
+    private bool disposed = false;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ArbitraryBitWriter"/> class.
+    /// </summary>
+    /// <param name="stream">Stream to write to.</param>
+    /// <param name="width">Width of integers between 4 and 32 bits.</param>
+    public ArbitraryBitWriter(Stream stream, int width)
+    {
+        ArgumentOutOfRangeException.ThrowIfLessThan(width, 4, nameof(width));
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(width, 32, nameof(width));
+
+        this.stream = stream;
+        this.width = width;
+        buffer = new byte[width * 8];
+        bitsWrittenInBuffer = 0;
+    }
+
+    /// <summary>
+    /// Writes <paramref name="number"/> to the underlying stream.
+    /// </summary>
+    /// <param name="number">Number to write.</param>
+    public void Write(int number)
+    {
+        ObjectDisposedException.ThrowIf(disposed, this);
+
+        number &= (int)(0xFFFFFFFF >> (32 - width));
+
+        int remainingWidth = width;
+        while (remainingWidth > 0)
+        {
+            int bufferOffset = bitsWrittenInBuffer / 8;
+            int bitsWrittenToCurrentByte = bitsWrittenInBuffer % 8;
+            int bitsRemainingInCurrentByte = 8 - bitsWrittenToCurrentByte;
+
+            int toWrite = number >> Math.Max(0, remainingWidth - bitsRemainingInCurrentByte);
+            int remainingWidthInCurrentByte = Math.Max(0, 8 - remainingWidth);
+            buffer[bufferOffset] |= (byte)(toWrite << remainingWidthInCurrentByte);
+
+            remainingWidth -= bitsRemainingInCurrentByte;
+            bitsWrittenInBuffer += bitsRemainingInCurrentByte - remainingWidthInCurrentByte;
+        }
+
+        if (bitsWrittenInBuffer == buffer.Length * 8)
+        {
+            stream.Write(buffer);
+        }
+    }
+
+    /// <summary>
+    /// Releases all resources used by the current instance of the <see cref="ArbitraryBitWriter"/> class.
+    /// </summary>
+    public void Dispose()
+    {
+        Dispose(true);
+    }
+
+    /// <summary>
+    /// Releases the unmanaged resources used by the <see cref="ArbitraryBitWriter"/> and optionally releases the managed resources.
+    /// </summary>
+    /// <param name="disposing"><see langword="true"/> to release both managed and unmanaged resources; <see langword="false"/> to release only unmanaged resources.</param>
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposed)
+        {
+            return;
+        }
+
+        disposed = true;
+
+        if (disposing)
+        {
+            return;
+        }
+
+        if (bitsWrittenInBuffer == 0)
+        {
+            return;
+        }
+
+        int bytesWrittenInBuffer = bitsWrittenInBuffer / 8;
+        stream.Write(buffer.AsSpan()[..(bytesWrittenInBuffer + 1)]);
+    }
+}
