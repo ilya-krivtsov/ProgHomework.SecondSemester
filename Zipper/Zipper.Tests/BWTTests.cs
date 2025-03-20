fb950497d43b1b922b0ee1826abc87564b1b4c67
ilya-krivtsov <180809461+ilya-krivtsov@users.noreply.github.com>
2025-03-20 16:39:26 +0300

    Updated tests for BWT (hw3 - lzw)

--- Zipper/Zipper.Tests/BWTTests.cs
+++ Zipper/Zipper.Tests/BWTTests.cs
@@ -25,8 +25,38 @@ public class BWTTests
         Span<byte> transformed = stackalloc byte[input.Length];
         var index = BWT.ForwardTransform(input, transformed);
 
-        Span<byte> reconstructed = stackalloc byte[input.Length];
+        Span<byte> reconstructed = stackalloc byte[input.Length + 16];
         BWT.InverseTransform(transformed, index, reconstructed);
-        Assert.That(reconstructed.SequenceEqual(input), Is.True);
+        Assert.That(reconstructed[..input.Length].SequenceEqual(input), Is.True);
+    }
+
+    [Test]
+    public void Transform_ShouldThrowIf_InputIsLargerThanOutput()
+    {
+        int inputLength = 16;
+        int outputLength = inputLength - 1;
+        Assert.Throws<ArgumentOutOfRangeException>(() => BWT.ForwardTransform(stackalloc byte[inputLength], stackalloc byte[outputLength]));
+    }
+
+    [Test]
+    public void InverseTransform_ShouldThrowIf_InputIsLargerThanOutput()
+    {
+        int inputLength = 16;
+        int outputLength = inputLength - 1;
+        Assert.Throws<ArgumentOutOfRangeException>(() => BWT.InverseTransform(stackalloc byte[inputLength], 0, stackalloc byte[outputLength]));
+    }
+
+    [Test]
+    public void InverseTransform_ShouldThrowIf_IdentityIndexIsNegative()
+    {
+        int length = 16;
+        Assert.Throws<ArgumentOutOfRangeException>(() => BWT.InverseTransform(stackalloc byte[length], -1, stackalloc byte[length]));
+    }
+
+    [Test]
+    public void InverseTransform_ShouldThrowIf_IdentityIndexIs_GreaterThanOrEqualTo_InputLength()
+    {
+        int length = 16;
+        Assert.Throws<ArgumentOutOfRangeException>(() => BWT.InverseTransform(stackalloc byte[length], length + 1, stackalloc byte[length]));
     }
 }
