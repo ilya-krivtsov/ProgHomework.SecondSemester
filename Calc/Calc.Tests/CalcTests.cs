e5445bb341fb395ac9298392996b7e288bfd62bc
ilya-krivtsov <180809461+ilya-krivtsov@users.noreply.github.com>
2025-05-26 09:06:37 +0300

    Homework 8 - Calc

---
+++ Calc/Calc.Tests/CalcTests.cs
@@ -0,0 +1,96 @@
+// <copyright file="CalcTests.cs" company="Ilya Krivtsov">
+// Copyright (c) Ilya Krivtsov. All rights reserved.
+// </copyright>
+
+namespace Calc.Tests;
+
+public class CalcTests
+{
+    private Calc calc;
+
+    [SetUp]
+    public void Setup()
+    {
+        calc = new();
+    }
+
+    [Test]
+    public void Test_2_Plus_3_Equals_5()
+    {
+        calc.EnterDigit(2);
+        Assert.That(calc.DisplayResult, Is.EqualTo("2"));
+
+        calc.OperatorPressed(ArithmeticOperation.Addition);
+        Assert.That(calc.DisplayResult, Is.EqualTo("2"));
+
+        calc.EnterDigit(3);
+        Assert.That(calc.DisplayResult, Is.EqualTo("3"));
+
+        calc.EqualsPressed();
+        Assert.That(calc.DisplayResult, Is.EqualTo("5"));
+    }
+
+    [Test]
+    public void Test_2_Plus_3_Minus_10_Equals_Minus_5()
+    {
+        calc.EnterDigit(2);
+        Assert.That(calc.DisplayResult, Is.EqualTo("2"));
+
+        calc.OperatorPressed(ArithmeticOperation.Addition);
+        Assert.That(calc.DisplayResult, Is.EqualTo("2"));
+
+        calc.EnterDigit(3);
+        Assert.That(calc.DisplayResult, Is.EqualTo("3"));
+
+        calc.OperatorPressed(ArithmeticOperation.Subtraction);
+        Assert.That(calc.DisplayResult, Is.EqualTo("5"));
+
+        calc.EnterDigit(1);
+        Assert.That(calc.DisplayResult, Is.EqualTo("1"));
+        calc.EnterDigit(0);
+        Assert.That(calc.DisplayResult, Is.EqualTo("10"));
+
+        calc.EqualsPressed();
+        Assert.That(calc.DisplayResult, Is.EqualTo("-5"));
+    }
+
+    [Test]
+    public void Test_0_Point_1_Plus_0_Point_2_Equals_0_Point_3()
+    {
+        calc.EnterDigit(0);
+        Assert.That(calc.DisplayResult, Is.EqualTo("0"));
+        calc.EnterDecimalPoint();
+        Assert.That(calc.DisplayResult, Is.EqualTo("0."));
+        calc.EnterDigit(1);
+        Assert.That(calc.DisplayResult, Is.EqualTo("0.1"));
+
+        calc.OperatorPressed(ArithmeticOperation.Addition);
+        Assert.That(calc.DisplayResult, Is.EqualTo("0.1"));
+
+        calc.EnterDigit(0);
+        Assert.That(calc.DisplayResult, Is.EqualTo("0"));
+        calc.EnterDecimalPoint();
+        Assert.That(calc.DisplayResult, Is.EqualTo("0."));
+        calc.EnterDigit(2);
+        Assert.That(calc.DisplayResult, Is.EqualTo("0.2"));
+
+        calc.EqualsPressed();
+        Assert.That(calc.DisplayResult, Is.EqualTo("0.3"));
+    }
+
+    [Test]
+    public void Test_0_Divide_0_Gives_Error()
+    {
+        calc.EnterDigit(0);
+        Assert.That(calc.DisplayResult, Is.EqualTo("0"));
+
+        calc.OperatorPressed(ArithmeticOperation.Division);
+        Assert.That(calc.DisplayResult, Is.EqualTo("0"));
+
+        calc.EnterDigit(0);
+        Assert.That(calc.DisplayResult, Is.EqualTo("0"));
+
+        calc.EqualsPressed();
+        Assert.That(calc.DisplayResult, Is.EqualTo("Error"));
+    }
+}
---
+++ Calc/Calc/Calc.cs
@@ -0,0 +1,198 @@
+// <copyright file="Calc.cs" company="Ilya Krivtsov">
+// Copyright (c) Ilya Krivtsov. All rights reserved.
+// </copyright>
+
+namespace Calc;
+
+using System.ComponentModel;
+using System.Globalization;
+
+/// <summary>
+/// Simple calc that can be used as backend for GUI calc.
+/// </summary>
+public class Calc : INotifyPropertyChanged
+{
+    private decimal leftOperand = 0;
+    private decimal rightOperand = 0;
+    private ArithmeticOperation operation;
+    private bool decimalPointPressed = false;
+    private decimal decimalMultiplier = 1;
+    private State state = State.EnteringFirstOperand;
+    private string displayResult = "0";
+
+    /// <inheritdoc/>
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private enum State
+    {
+        EnteringFirstOperand,
+        EnteringNewOperand,
+        PressedFirstOperator,
+        PressedOperator,
+        PressedEquals,
+        Error,
+    }
+
+    /// <summary>
+    /// Gets the result of calculations that can be displayed.
+    /// </summary>
+    public string DisplayResult
+    {
+        get => displayResult;
+        private set
+        {
+            displayResult = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(DisplayResult)));
+        }
+    }
+
+    /// <summary>
+    /// Enter a digit.
+    /// </summary>
+    /// <param name="digit">Digit to enter.</param>
+    public void EnterDigit(int digit)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(digit, nameof(digit));
+        ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(digit, 10, nameof(digit));
+
+        if (state == State.Error)
+        {
+            return;
+        }
+
+        if (state == State.PressedEquals)
+        {
+            leftOperand = 0;
+        }
+
+        state = state switch
+        {
+            State.EnteringFirstOperand => State.EnteringFirstOperand,
+            State.EnteringNewOperand => State.EnteringNewOperand,
+            State.PressedFirstOperator => State.EnteringNewOperand,
+            State.PressedOperator => State.EnteringNewOperand,
+            State.PressedEquals => State.EnteringFirstOperand,
+            _ => State.EnteringNewOperand,
+        };
+
+        ref var operand = ref state == State.EnteringFirstOperand ? ref leftOperand : ref rightOperand;
+
+        if (!decimalPointPressed)
+        {
+            operand = (operand * 10) + digit;
+        }
+        else
+        {
+            decimalMultiplier /= 10;
+            operand += decimalMultiplier * digit;
+        }
+
+        DisplayResult = operand.ToString(CultureInfo.InvariantCulture);
+    }
+
+    /// <summary>
+    /// Add decimal point to the number being entered.
+    /// </summary>
+    public void EnterDecimalPoint()
+    {
+        if (state == State.Error)
+        {
+            return;
+        }
+
+        if (state is State.EnteringFirstOperand or State.EnteringNewOperand)
+        {
+            decimalPointPressed = true;
+            DisplayResult += '.';
+        }
+    }
+
+    /// <summary>
+    /// Perform an arithmetic operation.
+    /// </summary>
+    /// <param name="operation">Operation to perform.</param>
+    public void OperatorPressed(ArithmeticOperation operation)
+    {
+        if (state == State.Error)
+        {
+            return;
+        }
+
+        if (decimalPointPressed)
+        {
+            decimalMultiplier = 1;
+            decimalPointPressed = false;
+        }
+
+        if (state == State.EnteringNewOperand)
+        {
+            PerformOperation();
+        }
+
+        state = state switch
+        {
+            State.EnteringFirstOperand => State.PressedFirstOperator,
+            State.EnteringNewOperand => State.PressedOperator,
+            State.PressedFirstOperator => State.PressedFirstOperator,
+            State.PressedOperator => State.PressedOperator,
+            State.PressedEquals => State.PressedOperator,
+            _ => State.PressedOperator,
+        };
+
+        this.operation = operation;
+    }
+
+    /// <summary>
+    /// Sets <see cref="DisplayResult"/> to the latest calculated value.
+    /// </summary>
+    public void EqualsPressed()
+    {
+        if (state == State.Error)
+        {
+            return;
+        }
+
+        if (state is State.EnteringNewOperand or State.PressedOperator)
+        {
+            PerformOperation();
+            if (state != State.Error)
+            {
+                state = State.PressedEquals;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Clears all calculations.
+    /// </summary>
+    public void Clear()
+    {
+        leftOperand = 0;
+        rightOperand = 0;
+        decimalMultiplier = 1;
+        decimalPointPressed = false;
+        state = State.EnteringFirstOperand;
+        DisplayResult = "0";
+    }
+
+    private void PerformOperation()
+    {
+        if (operation == ArithmeticOperation.Division && rightOperand == 0)
+        {
+            DisplayResult = "Error";
+            state = State.Error;
+            return;
+        }
+
+        leftOperand = operation switch
+        {
+            ArithmeticOperation.Addition => leftOperand + rightOperand,
+            ArithmeticOperation.Subtraction => leftOperand - rightOperand,
+            ArithmeticOperation.Multiplication => leftOperand * rightOperand,
+            ArithmeticOperation.Division => leftOperand / rightOperand,
+            _ => leftOperand,
+        };
+        rightOperand = 0;
+        DisplayResult = leftOperand.ToString(CultureInfo.InvariantCulture);
+    }
+}
